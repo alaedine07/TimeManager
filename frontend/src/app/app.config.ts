2ecb43d2791c410f17c1e38a55846cd2b0b70d15
alaedine07 <alaedineboufaden@gmail.com>
2025-10-31 19:01:54 +0100

    set data source to be mock

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { ProjectService } from './services/project.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    ProjectService,
+    {
+      provide: 'PROJECT_SERVICE_INIT',
+      useFactory: (ps: ProjectService) => {
+        ps.setDataSource('mock');
+        return null;
+      },
+      deps: [ProjectService]
+    }
   ]
 };
