46d7e6eb079bc16125b22401422395a6eb45df87
alaedine07 <alaedineboufaden@gmail.com>
2025-12-31 16:08:13 +0100

    feat: add script to aggregate project files into project.txt

---
+++ script.sh
@@ -0,0 +1,7 @@
+find backend frontend \
+  \( -path "*/migrations/*" -o -path "*/Migrations/*" -o -path "*/dist/*" -o -path "*/.angular/*" -o -path "*/obj/*" -o -path "*/public/*" \) -prune -o \
+  \( ! -name "taskmanagement.db" -a ! -name "package-lock.json" -type f -print \) \
+  | while read file; do
+      echo -e "\n===== FILE: $file =====\n" >> project.txt
+      cat "$file" >> project.txt
+    done
