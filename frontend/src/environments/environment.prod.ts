abb75ed6ac2b7b7f2e6e6424192e4e4b971b3203
alaedine07 <alaedineboufaden@gmail.com>
2026-01-09 14:02:35 +0100

    fix: update API URL in production environment configuration

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // frontend/src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'http://217.182.64.83:5000/api'
+  apiUrl: 'http://taskmanagerframework.alaspace.me/api'
 };
